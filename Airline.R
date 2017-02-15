6a07807a070238079b68acf60efe25013d4674a2
kti25 <kevin.rossi@outlook.com>
2017-02-15 14:16:25 -0500

    Add files via upload

---
+++ Airline.R
@@ -0,0 +1,76 @@
+
+library(dplyr)
+library(tidyverse)
+
+#import files
+getwd()
+setwd("C:/Users/Kevin Rossi/Desktop/R/")
+
+q1 = read.csv("q1_16_sample.csv", sep = " ", header = TRUE)
+q11<-data.frame(q1)
+head(q11)
+names(q11)
+dim(q11)
+q2 = read.csv("q2_16_sample.csv", sep = " ", header = TRUE)
+q22<-data.frame(q2)
+head(q22)
+names(q22)
+dim(q22)
+q3 = read.csv("q3_16_sample.csv", sep = " ", header = TRUE)
+q33<-data.frame(q3)
+head(q33)
+names(q33)
+dim(q33)
+q4 = read.csv("q4_15_sample.csv", sep = " ", header = TRUE)
+q44<-data.frame(q4)
+head(q44)
+names(q44)
+dim(q44)
+
+#combine files
+new<-rbind.data.frame(q11,q22,q33,q44)
+names(new)
+dim(new)
+head(new)
+attach(new)
+
+#create new feature: origin and destination state and city names; find a file online to combine with
+state = read.csv("states.csv", header = TRUE)
+city = read.csv("airportcity.csv", header = TRUE)
+aero = read.csv("aerodata.csv", header = TRUE)
+
+air1<-mutate(new, ORIGIN_STATE = aero$state[match(ORIGIN_AIRPORT_ID, aero$id)])
+air2<-mutate(air1, ORIGIN_CITY = aero$city[match(ORIGIN_AIRPORT_ID, aero$id)])
+air3<-mutate(air2, DEST_STATE = aero$state[match(DEST_AIRPORT_ID, aero$id)])
+air4<-mutate(air3, DEST_CITY = aero$city[match(DEST_AIRPORT_ID, aero$id)])
+names(air4)
+head(air4)
+summary(air4)
+dim(air4)
+#tried including non-us and fixed some of missing - 2751 for dest, 2707 for origin
+
+#new feature: categorical (4 cat.) price features - MARKET_FARE
+#0-<=125,>125-<=250,>250-<=375,>375
+air4$PRICE_CAT <- cut(air4$MARKET_FARE,
+                     breaks=c(-Inf, 125, 250, 375, Inf),
+                     labels=c("Lowest","Low","Medium","High"))
+summary(air4)
+
+# how many fairs were one way?
+x = count(air4, c('ITIN_ID'))
+dim(x)
+f<-filter(x, x$freq> 1)
+dim(f)
+
+#how often did ticket carrier change? (op_carrier_change)
+# binary - change if 1, no change if not. count how many their were total
+w = table(air4$OP_CARRIER_CHANGE)
+w
+
+#run basic discriptive stats and provide one unique insight
+summary(air4)
+dim(air4)
+head(air4)
+str(air4)
+#people who have "bulk_fare" do not pay anything for "market_fare" 
+
